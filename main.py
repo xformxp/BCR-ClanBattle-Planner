21e3e5d5b836a16bbab591d43972a3835e2d5396
EMP <xformxp.emp@gmail.com>
2022-06-25 20:35:13 +0800

    Jun 2022

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 from tkinter import *
 from tkinter.filedialog import *
 import itertools
+from collections import defaultdict
 import _pcr_data_202204 as _pcr_data
 import re
 import requests
@@ -96,10 +97,17 @@ class Window:
         self.gen_choice.grid(row=1, column=0, padx=5, pady=5)
         self.l_choice.grid(row=1, column=2, pady=5)
         self.f_choice.grid(row=1, column=1)
-        self.JPN = IntVar(value=0)
         self.l_stage = Button(self.F1, text=f"{self.stage}阶段（切换）", command = self.changeStage)
         self.l_stage.grid(row=0, column=0, pady=5)
-        Checkbutton(self.F1, text="谜语人模式", variable=self.JPN).grid(row=2, column=0, columnspan=3, pady=5)
+        def trunc(var: StringVar):
+            c = var.get()[-3:]
+            var.set(c)
+        self.boss_choice = StringVar(value = "111")
+        self.boss_choice.trace_add("write", lambda x,y,z,c=self.boss_choice:trunc(c))
+        self.JPN = IntVar(value=0)
+        Label(self.F1, text="选择boss：").grid(row=2, column=0, pady=5)
+        Entry(self.F1, textvariable=self.boss_choice, width=3).grid(row=2, column=1, pady=5)
+        Checkbutton(self.F1, text="谜语人模式", variable=self.JPN).grid(row=2, column=2, pady=5)
         button = Button(self.F1, text="计算", command=self.calc)
         button.grid(row=3, column=0, columnspan=3, pady=5)
         self.result = Label(self.F2, text="", font=('TkDefaultFont', 12))
@@ -134,7 +142,7 @@ class Window:
             for ch in box_read:
                 filtered_box.append(chara.getCharaId(ch))
             self.box = set(chara.getCharaList()) - set(filtered_box)
-        self.choice = []
+        self.choice = defaultdict(list)
         with open(self.choice_file, "r", encoding="utf-8") as f:
             choices = f.read().strip().split('\n')
             for choice in choices:
@@ -143,13 +151,15 @@ class Window:
                 if len(c) != 7 or not c[6].isdigit():
                     self.writeResult("作业文件格式错误")
                     return
-                self.choice.append({"name": c[0], "team": list(map(chara.getCharaId, c[1:6])), "dmg": int(c[6])})
+                self.choice[c[0][2]].append({"name": c[0], "team": list(map(chara.getCharaId, c[1:6])), "dmg": int(c[6])})
         res = []
         output = []
-        for perm in itertools.combinations(self.choice, 3):
-            avail, dmg = self.checkAvailability(perm)
-            if avail:
-                res.append((dmg, perm))
+        boss_choices = ''.join(sorted(self.boss_choice.get()))
+        for perm in itertools.product(*[self.choice[boss_choices[i]] for i in range(3)]):
+            if(perm[2]["name"] > perm[1]["name"] and perm[1]["name"] > perm[0]["name"]):
+                avail, dmg = self.checkAvailability(perm)
+                if avail:
+                    res.append((dmg, perm))
         res.sort(key=lambda x: x[0], reverse=True)
         if len(res) == 0:
             self.writeResult('没有可用分刀')
@@ -165,7 +175,7 @@ class Window:
         self.writeResult('\n\n'.join(output[:5]))
 
     def checkAvailability(self, perm: Tuple[dict, dict, dict]) -> Tuple[bool, int]:
-        box_copy = self.box
+        #print(perm[0], perm[1], perm[2])
         team = [x["team"] for x in perm]
         for i, j, k in itertools.product(itertools.combinations(team[0], 4),
                                          itertools.combinations(team[1], 4),
